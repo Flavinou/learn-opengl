b30ff065679b2287291c0f4b682bdc28d4088891
Flavien Febvay <flavien.febvay@ubisoft.com>
2024-10-03 16:07:10 +0200

    feat: add GLEW dependency + initial setup to draw a triangle
    * GLEW is a cross-platform loading library that provides a mechanism for determining which OpenGL extensions are supported on the target platform - by our GPU manufacturer

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -19,18 +22,32 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
+
+    std::cout << "OpenGL " << glGetString(GL_VERSION) << std::endl;
+
+    /* Define 3 vertices as an array of floats that will make up a triangle once linked together */
+    float positions[6] = {
+       -0.5f, -0.5f,
+        0.0f,  0.5f,
+        0.5f, -0.5f
+    };
+
+    /* Tell OpenGL that we need to store these positions as an array and select them to be drawn on screen */
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Debug - Draw a triangle in the middle of the screen */
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        /* Supposedly draw a triangle BUT how the memory is laid out is still missing, OpenGL doesn't know how to render those vertices yet */
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
