eba5f6d80a9279ee010144ed76d8843a4d4908d8
Flavien Febvay <flavien.febvay@ubisoft.com>
2024-11-13 15:10:59 +0100

    feat: simply rendering multiple objects
    - It happens by providing copies of the same model view projection matrices and rendering the same buffers twice, however this happens via separate draw calls which is not ideal at all when rendering thousands of objects

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -53,10 +53,10 @@ int main()
     {
         /* Define 3 vertices as an array of floats that will make up a triangle once linked together */
         float positions[] = {
-           100.5f, 100.5f, 0.0f, 0.0f, // 0
-           200.5f, 100.5f, 1.0f, 0.0f, // 1
-           200.5f, 200.5f, 1.0f, 1.0f, // 2
-           100.5f, 200.5f, 0.0f, 1.0f  // 3
+            -50.0f, -50.0f, 0.0f, 0.0f, // 0
+             50.0f, -50.0f, 1.0f, 0.0f, // 1
+             50.0f,  50.0f, 1.0f, 1.0f, // 2
+            -50.0f,  50.0f, 0.0f, 1.0f  // 3
         };
 
         unsigned int indices[] = {
@@ -84,7 +84,7 @@ int main()
 
         /* Projection matrix - map 3D world to 2D screen space - specifying here a 4/3 aspect ratio */
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
@@ -114,7 +114,8 @@ int main()
         float r = 0.0f;
         float increment = 0.05f;
 
-        glm::vec3 translation = glm::vec3(200, 200, 0);
+        glm::vec3 translationA(200, 200, 0);
+        glm::vec3 translationB(400, 200, 0);
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
@@ -127,15 +128,24 @@ int main()
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-            glm::mat4 mvp = proj * view * model;
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                shader.SetUniformMat4f("u_MVP", mvp);
 
-            shader.Bind();
-            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-            shader.SetUniformMat4f("u_MVP", mvp);
+                renderer.Draw(va, ib, shader);
+            }
 
-            /* Render triangles from given indices above */
-            renderer.Draw(va, ib, shader);
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                shader.SetUniformMat4f("u_MVP", mvp);
+
+                renderer.Draw(va, ib, shader);
+            }
+
 
             if (r > 1.0f)
                 increment = -0.05f;
@@ -146,7 +156,8 @@ int main()
 
             /* ImGui - begin new window */
             {
-                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+                ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
+                ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
 
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
             }
